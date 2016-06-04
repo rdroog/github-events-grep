3519c75668628917c743dabcf0d0fc70721f89d0
rdroog <renzedroog@gmail.com>
2016-06-04 21:20:14 +0200

    Added one possible API call in the simplest form: /regexp matches the regexp on some data in the GitHub Events API.

--- src/github.js
+++ src/github.js
@@ -8,19 +8,22 @@ const request = require('request');
 const hostname = '127.0.0.1';
 const port = 1337;
 
-const PUSHEVENT = 'PushEvent';
 
 http.createServer((req, res) => {
+    console.log('server started');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    makeGithubConnection();
-    //res.end('Hello World\n');
+    var regexp = new RegExp(req.url.substr(1));
+    var results = makeGithubConnection(regexp, res);
+    //res.end(results);
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-function makeGithubConnection() {
+function makeGithubConnection(regexp, res) {
+    console.log('trying to make connection to github...');
     const options = {
-        url: 'https://api.github.com/users/rdroog/events/public',
+        //url: 'https://api.github.com/users/rdroog/events/public',
+        url: 'https://api.github.com/events',
         timeout: 5000,
         headers: {
             'User-Agent': 'github-events-grep'
@@ -33,39 +36,91 @@ function makeGithubConnection() {
         get(options).
         on('error', function(err) {
             if(err.code === 'ETIMEDOUT') {
-                //console.log('timeout at server');
+                console.log('timeout at github');
             } else {
-                //console.log('error occurred: ' + err);
+                console.log('error occurred at github: ' + err);
             }
         }).
         on('response', function(response) {
             if(response.statusCode === 200) {
-                //console.log('status code correct');
+                console.log('github status code correct');
             } else {
-                //console.log('incorrect status code: ' + response.statusCode);
+                console.log('incorrect github status code: ' + response.statusCode);
             }
         }).
         on('data', function(chunk) {
-            console.log('received: ' + chunk);
+            console.log('received data');
             data += chunk;
         }).
         on('end', function() {
-            filterOnRegex(data, regex);
-            console.log('----------------end------------------------------');
+            filterOnRegexp(data, regexp, res);
+            console.log('-----end of data-----');
         });
+    console.log('request send');
 }
 
-function filterOnRegex(data, regex) {
-    var results = [];
+function filterOnRegexp(data, regexp, res) {
     console.log('filteronregexp');
+    var results = [];
     const events = JSON.parse(data);
+    
     var stream = events.
-        filter(function(event) {
-            return event.type == PUSHEVENT;
-        }).
-        map(function(event) {
-            results += event.payload;
-            console.log('done');
+        forEach(function(event) {
+            var matched = false;
+            
+            if(event.type === 'CommitCommentEvent' || event.type === 'IssueCommentEvent' || event.type === 'PullRequestReviewCommentEvent') {
+                matched = matched || regexp.test(event.payload.comment.body);
+            } else if(event.type === 'CreateEvent') {
+                matched = matched || regexp.test(event.payload.description);
+            } else if(event.type === 'DeleteEvent') {
+                ;
+            } else if(event.type === 'DeploymentEvent') {
+                ;
+            } else if(event.type === 'DeploymentStatusEvent') {
+                ;
+            } else if(event.type === 'ForkEvent') {
+                ;
+            } else if(event.type === 'GollumEvent') {
+                event.payload.pages.
+                    forEach(function(page) {
+                        matched = matched || regexp.test(page.summary);
+                    });
+            } else if(event.type === 'IssuesEvent') {
+                matched = matched || regexp.test(event.payload.action);
+            } else if(event.type === 'MemberEvent') {
+                ;
+            } else if(event.type === 'MembershipEvent') {
+                ;
+            } else if(event.type === 'PageBuildEvent') {
+                ;
+            } else if(event.type === 'PublicEvent') {
+                ;
+            } else if(event.type === 'PullRequestEvent') {
+                ;
+            } else if(event.type === 'PushEvent') {
+                event.payload.commits.
+                    forEach(function(commit) {
+                        matched = matched || regexp.test(commit.message);
+                    });
+            } else if(event.type === 'ReleaseEvent') {
+                ;
+            } else if(event.type === 'RepositoryEvent') {
+                ;
+            } else if(event.type === 'TeamAddEvent') {
+                ;
+            } else if(event.type === 'WatchEvent') {
+                ;
+            } else {
+                console.log('Event type not used: ' + event.type);
+            }
+            
+            if(matched) {
+                console.log('match');
+                results.push(event);
+            } else {
+                console.log('no match');
+            }
         });
-    console.log(results);
+    
+    res.end(JSON.stringify(results, null, '  '));
 }
