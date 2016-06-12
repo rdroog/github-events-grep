9195495b2ce9abc344d10140d9f0faab3f4980a9
rdroog <renzedroog@gmail.com>
2016-06-12 17:18:49 +0200

    Added information about amount of matches per event, changed some logs, created separate 'getRegexp()' function

--- src/github.js
+++ src/github.js
@@ -10,20 +10,31 @@ const port = 1337;
 
 
 http.createServer((req, res) => {
-    console.log('server started');
+    console.log('request received from client');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    var regexp = new RegExp(req.url.substr(1));
+    
+    regexp = getRegexp(req.url);
+    
     var results = makeGithubConnection(regexp, res);
     //res.end(results);
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+function getRegexp(url) {
+    var regexpstr = url.substr(1);
+    var regexp = new RegExp(regexpstr, 'i');
+    
+    console.log('regexp = ' + regexpstr);
+    
+    return regexp;
+}
+
 function makeGithubConnection(regexp, res) {
     console.log('trying to make connection to github...');
     const options = {
-        //url: 'https://api.github.com/users/rdroog/events/public',
-        url: 'https://api.github.com/events',
+        url: 'https://api.github.com/users/rdroog/events/public',
+        //url: 'https://api.github.com/events',
         timeout: 5000,
         headers: {
             'User-Agent': 'github-events-grep'
@@ -43,7 +54,7 @@ function makeGithubConnection(regexp, res) {
         }).
         on('response', function(response) {
             if(response.statusCode === 200) {
-                console.log('github status code correct');
+                console.log('github status code correct (' + response.statusCode + ')');
             } else {
                 console.log('incorrect github status code: ' + response.statusCode);
             }
@@ -56,7 +67,7 @@ function makeGithubConnection(regexp, res) {
             filterOnRegexp(data, regexp, res);
             console.log('-----end of data-----');
         });
-    console.log('request send');
+    console.log('request sent to Github');
 }
 
 function filterOnRegexp(data, regexp, res) {
@@ -67,11 +78,14 @@ function filterOnRegexp(data, regexp, res) {
     var stream = events.
         forEach(function(event) {
             var matched = false;
+            var matches = 0;
             
             if(event.type === 'CommitCommentEvent' || event.type === 'IssueCommentEvent' || event.type === 'PullRequestReviewCommentEvent') {
                 matched = matched || regexp.test(event.payload.comment.body);
+                matches += regexp.test(event.payload.comment.body);
             } else if(event.type === 'CreateEvent') {
                 matched = matched || regexp.test(event.payload.description);
+                matches += regexp.test(event.payload.description);
             } else if(event.type === 'DeleteEvent') {
                 ;
             } else if(event.type === 'DeploymentEvent') {
@@ -84,9 +98,11 @@ function filterOnRegexp(data, regexp, res) {
                 event.payload.pages.
                     forEach(function(page) {
                         matched = matched || regexp.test(page.summary);
+                        matches += regexp.test(page.summary);
                     });
             } else if(event.type === 'IssuesEvent') {
                 matched = matched || regexp.test(event.payload.action);
+                matches += regexp.test(event.payload.action);
             } else if(event.type === 'MemberEvent') {
                 ;
             } else if(event.type === 'MembershipEvent') {
@@ -101,6 +117,7 @@ function filterOnRegexp(data, regexp, res) {
                 event.payload.commits.
                     forEach(function(commit) {
                         matched = matched || regexp.test(commit.message);
+                        matches += regexp.test(commit.message);
                     });
             } else if(event.type === 'ReleaseEvent') {
                 ;
@@ -116,9 +133,11 @@ function filterOnRegexp(data, regexp, res) {
             
             if(matched) {
                 console.log('match');
+                console.log(matches);
                 results.push(event);
             } else {
                 console.log('no match');
+                console.log(matches);
             }
         });
     
