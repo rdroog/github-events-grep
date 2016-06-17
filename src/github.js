14a392ea9be8382907cb34605da64a5c7074440a
rdroog <renzedroog@gmail.com>
2016-06-17 21:15:17 +0200

    Added simple logging utility with customized logging levels.

--- src/github.js
+++ src/github.js
@@ -9,6 +9,8 @@ const run = require('gen-run');
 const hostname = '127.0.0.1';
 const port = 1337;
 
+const loggingLevel = 9; // 0 = nothing, 4 = some, 9 = all
+
 /* Deprecated because otherwise it cannot handle more than 1 request concurrently. */
 /* Parts on which the call should be executed*/
 //var custom = false;
@@ -30,20 +32,22 @@ const eventsURL = 'https://api.github.com/events?per_page=100';
 const timeout = 5000;
 
 http.createServer((req, res) => {
-    console.log('request received from client');
+    logger(0, 'request received from client');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     
     APIInfo = getAPIInfo(req.url);
-    console.log('APIInfo received');
+    logger(9, 'APIInfo received');
     
     filterEvents(APIInfo, res);
 }).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    logger(0, `Server running at http://${hostname}:${port}/`);
     
     nextGithubRequestAt = Date.now();
     
     startGithubConnection();    
 });
+
+/* FUNCTIONS EXECUTED PER REQUEST */
 
 function getAPIInfo(url) {
     const path = url.substr(1);
@@ -67,25 +71,25 @@ function getAPIInfo(url) {
         standard = true;
     }
     
-    console.log('part = ' + part);
+    logger(9, 'part = ' + part);
     
     const indexCallEnd = path.indexOf('/', indexPartEnd+1);
     const call = path.substr(indexPartEnd+1, indexCallEnd-indexPartEnd-1);
     
     if(call === ALLEVENTS) {
         APICall = ALLEVENTS;
-        console.log('apicall = ' + APICall);
+        logger(9, 'apicall = ' + APICall);
         regexpstr = path.substr(indexCallEnd+1);
     } else if (call === ONEEVENT) {
         APICall = ONEEVENT;
         const indexEventEnd = path.indexOf('/', index+1);
         APIEvent = path.substr(indexCallEnd+1, indexEventEnd-indexCallEnd-1);
         
-        console.log('path = ' + path);
-        console.log('index = ' + index);
-        console.log('index2 = ' + index2);
-        console.log('apicall = ' + APICall);
-        console.log('apievent = ' + APIEvent);
+        logger(9, 'path = ' + path);
+        logger(9, 'index = ' + index);
+        logger(9, 'index2 = ' + index2);
+        logger(9, 'apicall = ' + APICall);
+        logger(9, 'apievent = ' + APIEvent);
         
         regexpstr = path.substr(indexEventEnd+1);
     } else {
@@ -106,99 +110,9 @@ function getAPIInfo(url) {
 function getRegexp(regexpstr) {
     var regexp = new RegExp(regexpstr, 'i');
     
-    console.log('regexp = ' + regexpstr);
+    logger(9, 'regexp = ' + regexpstr);
     
     return regexp;
-}
-
-function startGithubConnection() {
-    console.log('trying to make connection to github...');
-    
-    options = getOptions();
-    
-    nextGithubRequest(options);
-}
-
-function getOptions() {
-    var options;
-    
-    if(etag) {
-        options = {
-            url: eventsURL,
-            timeout: timeout,
-            headers: {
-                'User-Agent': 'github-events-grep',
-                'ETag': etag
-            }
-        };
-    } else {
-        options = {
-            url: eventsURL,
-            timeout: timeout,
-            headers: {
-                'User-Agent': 'github-events-grep'
-            }
-        };
-    }
-    
-    return options;
-}
-
-function nextGithubRequest(options) {
-    var data = [];
-    
-    run(function* (gen) {
-        const sleepFor = Math.max(0, nextGithubRequestAt - Date.now());
-        
-        yield setTimeout(gen(), sleepFor);
-        
-        request.
-            get(options).
-            on('error', function(err) {
-                if(err.code === 'ETIMEDOUT') {
-                    console.log('timeout at github');
-                } else {
-                    console.log('error occurred at github: ' + err);
-                }
-            }).
-            on('response', function(response) {
-                if(response.statusCode === 200) {
-                    console.log('github status code correct (' + response.statusCode + ')');
-                } else {
-                    console.log('incorrect github status code: ' + response.statusCode);
-                }
-                
-                getNextGithubRequestAt(response.headers);
-                etag = response.headers['etag'];
-            }).
-            on('data', function(chunk) {
-                //console.log('received data');
-                data += chunk;
-            }).
-            on('end', function() {
-                var newevents = JSON.parse(data);
-                allevents = allevents.concat(newevents);
-                console.log('-----end of data-----');
-                
-                options = getOptions();
-                //nextGithubRequest(options);
-            });
-        console.log('request sent to Github');
-    });
-}
-
-function getNextGithubRequestAt(headers) {
-    const pollinterval = headers['x-poll-interval']; // in seconds
-            
-    const rateRemaining = headers['x-ratelimit-remaining']; // until rateReset
-    const rateReset = new Date(headers['x-ratelimit-reset'] * 1000); // in ms
-    const now = Date.now();
-    
-    const ratePerMs = Math.max(pollinterval/1000, (rateReset - now) / rateRemaining);
-    
-    nextGithubRequestAt = now + ratePerMs;
-    
-    //console.log('next github request at: ' + nextGithubRequestAt.toUTCString()  + ' (UTC)');
 }
 
 function filterEvents(APIInfo, res) {
@@ -206,7 +120,7 @@ function filterEvents(APIInfo, res) {
     var events = JSON.parse(JSON.stringify(allevents));
     
     if(APIInfo.APICall === ONEEVENT) {
-        console.log('going');
+        logger(9, 'going');
         events = events.
             filter(function(event) {
                 return event.type === APIInfo.APIEvent;
@@ -217,7 +131,7 @@ function filterEvents(APIInfo, res) {
 }
 
 function filterOnRegexp(events, APIInfo, res) {
-    console.log('filteronregexp');
+    logger(9, 'filteronregexp');
     var results = [];
     
     regexp = APIInfo.regexp;
@@ -300,7 +214,7 @@ function filterOnRegexp(events, APIInfo, res) {
                     result = regexp.test(event.payload.action);
                     matched = matched || result;
                 } else {
-                    console.log('Event type not used: ' + event.type);
+                    logger(0, 'Event type not used: ' + event.type);
                 }
             }
             
@@ -315,12 +229,118 @@ function filterOnRegexp(events, APIInfo, res) {
              
             // If this event is matched, add it to results
             if(matched) {
-                console.log('match');
+                logger(9, 'match');
                 results.push(event);
             } else {
-                console.log('no match');
+                logger(9, 'no match');
             }
         });
     
     res.end(JSON.stringify(results, null, '  '));
+}
+
+/* FUNCTIONS EXECUTED PER SERVER */
+
+// Starts the connection to Github.
+function startGithubConnection() {
+    logger(9, 'trying to make connection to github...');
+    
+    options = getOptions();
+    
+    nextGithubRequest(options);
+}
+
+// Gets the options depending on if etag is known.
+function getOptions() {
+    var options;
+    
+    if(etag) {
+        options = {
+            url: eventsURL,
+            timeout: timeout,
+            headers: {
+                'User-Agent': 'github-events-grep',
+                'ETag': etag
+            }
+        };
+    } else {
+        options = {
+            url: eventsURL,
+            timeout: timeout,
+            headers: {
+                'User-Agent': 'github-events-grep'
+            }
+        };
+    }
+    
+    return options;
+}
+
+// Will poll Github continuously based on its limits
+function nextGithubRequest(options) {
+    var data = [];
+    
+    run(function* (gen) {
+        const sleepFor = Math.max(0, nextGithubRequestAt - Date.now());
+        
+        yield setTimeout(gen(), sleepFor);
+        
+        request.
+            get(options).
+            on('error', function(err) {
+                if(err.code === 'ETIMEDOUT') {
+                    logger(0, 'timeout at github');
+                } else {
+                    logger(0, 'error occurred at github: ' + err);
+                }
+            }).
+            on('response', function(response) {
+                if(response.statusCode === 200) {
+                    logger(1, 'github status code correct (' + response.statusCode + ')');
+                } else {
+                    logger(0, 'incorrect github status code: ' + response.statusCode);
+                }
+                
+                getNextGithubRequestAt(response.headers);
+                etag = response.headers['etag'];
+            }).
+            on('data', function(chunk) {
+                logger(9, 'received data');
+                data += chunk;
+            }).
+            on('end', function() {
+                var newevents = JSON.parse(data);
+                allevents = allevents.concat(newevents);
+                logger(9, '-----end of data-----');
+                
+                options = getOptions();
+                //nextGithubRequest(options);
+            });
+        logger(4, 'request sent to Github');
+    });
+}
+
+// Calculates when the server should ask Github again for more information.
+// Based on both the x-poll-interval option and x-ratelimit options.
+function getNextGithubRequestAt(headers) {
+    const pollinterval = headers['x-poll-interval']; // in seconds
+            
+    const rateRemaining = headers['x-ratelimit-remaining']; // rate amount left until rateReset
+    const rateReset = new Date(headers['x-ratelimit-reset'] * 1000); // in ms
+    const now = Date.now();
+    
+    const ratePerMs = Math.max(pollinterval/1000, (rateReset - now) / rateRemaining);
+    
+    nextGithubRequestAt = now + ratePerMs;
+    
+    const nextDate = new Date(nextGithubRequestAt);
+    
+    logger(9, 'next github request at: ' + nextDate.toUTCString()  + ' (UTC)');
+}
+
+
+function logger(level, str) {
+    if(loggingLevel >= level) {
+        console.log(str);
+    }
 }
